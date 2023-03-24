2a9dedb7a74e3eaadf2577fa789f53f194522824
Hosivi <85600470+Hosivi@users.noreply.github.com>
2023-03-24 09:21:33 -0500

    version para uso con messaging center

---
+++ SiviComponents/Services/MessagingService.cs
@@ -0,0 +1,33 @@
+﻿namespace SiviComponents.Services;
+
+public delegate Task QueryMessageHandler(string query, Action afterAceptAction);
+public interface IMessagingService
+{
+      
+    public event QueryMessageHandler MessageSending; 
+    Task<TMessage> SendMessage<TMessage>(TMessage message);
+    Task SendQueryMessage(string query,Action afterAceptAction); 
+    
+
+}
+
+public class MessagingService : IMessagingService
+{
+    public event QueryMessageHandler MessageSending;
+    public Task<TMessage> SendMessage<TMessage>(TMessage message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task SendQueryMessage(string query, Action afterAceptAction)
+    {
+	    if (MessageSending != null)
+	    {
+		    await MessageSending(query, afterAceptAction);
+		}
+	    else
+	    {
+		    Console.WriteLine("aaaaaaaaaaaaaaaaaaa no te haz suscrioto");
+	    }
+    }
+}
---
+++ SiviComponents/Services/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SiviComponents.Services;
+
+// namespace MessagingService;
+//
+// public static class ServiceExtensions
+// {
+//     public static IServiceCollection AddMessagingService(this IServiceCollection services)
+//     {
+//         services.AddSingleton
+// 	        <IMessagingService,SiviComponents.Services.MessagingService>(); 
+//         return services; 
+//     }
+// }
